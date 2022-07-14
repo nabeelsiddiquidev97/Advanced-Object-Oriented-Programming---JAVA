d7ad8a5f9f3224feae8cbdb2c73604700b72afeb
Nabeel Ahmed Siddiqui <101038092+nabeelsiddiquidev97@users.noreply.github.com>
2022-07-14 19:04:50 +0500

    Add files via upload
    Inheritance Using Swing Button & Event Handling.
    Objective: Creating Button using JButton. Run this in Netbeans or Eclipse.

---
+++ JButton Code/FirstSwingNab.java
@@ -0,0 +1,20 @@
+package firstswingnab;
+
+import javax.swing.*;
+/**
+ *
+ * @author Nabeel Ahmed Siddiqui - https://github.com/nabeelsiddiquidev97
+ */
+
+public class FirstSwingNab {
+
+  public static void main(String[] args) {
+JFrame f=new JFrame();//creating instance of JFrame  
+JButton b=new JButton("click me");//creating instance of JButton  
+b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+f.add(b);//adding button in JFrame        
+f.setSize(400,500);//400 width and 500 height  
+f.setLayout(null);//using no layout managers  
+f.setVisible(true);//making the frame visible  
+}  
+}  
---
+++ JButton Code/readme.txt
@@ -0,0 +1,2 @@
+Inheritance Using Swing Button & Event Handling.
+Objective: Creating Button using JButton. Run this in Netbeans or Eclipse.
